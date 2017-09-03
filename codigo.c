5c95999e59fcb1c6997854873c410586e6e3a448
Wagner <wmariano@id.uff.br>
2017-09-03 19:07:59 -0300

    Iniciando arquivo e montando função imprime

--- codigo.c
+++ codigo.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
 
+void imprime(float v1[], float v2[], float v3[], float v4[])
+{
+	int i;
+
+		printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+			printf("%.1f \t", v1[i]);
+			}
+
+				printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+			printf("%.1f \t", v2[i]);
+			}
+
+				printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+			printf("%.1f \t", v3[i]);
+			}
+
+				printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+			printf("%.1f \t", v4[i]);
+			}
+
+				printf("\n");
+}
+
+main() {
+
+}
