6f4ba8bfb399f9429310a4a5a21d668552531e5e
Wagner <wmariano@id.uff.br>
2017-09-03 19:13:34 -0300

    Lendo a matriz e imprimindo

--- codigo.c
+++ codigo.c
@@ -37,4 +37,46 @@ void imprime(float v1[], float v2[], float v3[], float v4[])
 
 main() {
 
+int i;
+float K;
+float x1, x2, x3, x4;
+
+	float m1[5], m2[5], m3[5], m4[5], ma[5];
+
+
+		for(i=0; i<5; i++)
+			{
+				printf("digite os valores da primeira linha na posição [%d]: ", i);
+				scanf("%f", &m1[i]);
+			}
+
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("digite os valores da segunda linha na posição [%d]: ", i);
+				scanf("%f", &m2[i]);
+			}
+
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("digite os valores da terceira linha na posição [%d]: ", i);
+				scanf("%f", &m3[i]);
+			}
+
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("digite os valores da quarta linha na posição [%d]: ", i);
+				scanf("%f", &m4[i]);
+			}
+
+			printf("\n");
+
+imprime(m1,m2,m3,m4);
+
+
 }
